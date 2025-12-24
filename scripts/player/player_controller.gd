2b070145ae679e169bf2a5c4baac9e77a60f047c
TikiTop <cpttiki@gmail.com>
2025-12-24 10:00:15 -0500

    Camera Juice
    Screenshake
    Headbob
    Recoil
    Movement Tilt

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -20,17 +20,22 @@ class_name PlayerController
 @export var crouch_speed_mod : float = -4
 @export_category("Jump Settings")
 @export var jump_velocity : float = 5
+@export var fall_velocity_threshold : float = -5.0
+@export_category("Data Helpers")
+@export var data_relative_velocity : Vector3
+
 var _input_dir : Vector2 = Vector2.ZERO
 var _movement_velocity : Vector3 = Vector3.ZERO
-var sprint_modifier : float = 0.0
-var crouch_modifier : float = 0.0
+var _sprint_modifier : float = 0.0
+var _crouch_modifier : float = 0.0
+var current_fall_velocity : float
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
 	_input_dir = Input.get_vector("move_left", "move_right", "move_fwd", "move_back")
-	var speed = default_speed + sprint_modifier + crouch_modifier
+	var speed = default_speed + _sprint_modifier + _crouch_modifier
 	var current_velocity : Vector2 = Vector2(_movement_velocity.x, _movement_velocity.z)
 	var direction : Vector3 = (transform.basis * Vector3(_input_dir.x, 0.0, _input_dir.y)).normalized()
 	if _input_dir.length() > 0.0:
@@ -50,19 +55,28 @@ func update_rotation(rotation_input : Vector3) -> void:
 	global_transform.basis = Basis.from_euler(rotation_input)
 
 func sprint() -> void:
-	sprint_modifier = sprint_speed_mod
+	_sprint_modifier = sprint_speed_mod
 
 func walk() -> void:
-	sprint_modifier = 0.0
+	_sprint_modifier = 0.0
 
 func crouch() -> void:
-	crouch_modifier = crouch_speed_mod
+	_crouch_modifier = crouch_speed_mod
 	crouch_collision.disabled = false
 	standing_collision.disabled = true
 
 func stand() -> void:
+	_crouch_modifier = 0
 	crouch_collision.disabled = true
 	standing_collision.disabled = false
 
 func jump() -> void:
 	velocity.y += jump_velocity
+
+func check_fall_speed() -> bool:
+	if current_fall_velocity < fall_velocity_threshold:
+		current_fall_velocity = 0.0
+		return true
+	else:
+		current_fall_velocity = 0.0
+		return false
--- scripts/player/states/player_airborne_state.gd
+++ scripts/player/states/player_airborne_state.gd
@@ -5,4 +5,6 @@ class_name PlayerAirbornState
 
 func _on_airborne_state_physics_processing(_delta: float) -> void:
 	if player_controller.is_on_floor():
+		if player_controller.check_fall_speed():
+			player_controller.camera_effects.add_fall_kick(player_controller.camera_effects.fall_kick_strength)
 		player_controller.state_chart.send_event("onGrounded")
